abaaa1b4aa8ea968c422cc194ee1baadbb9b084d
daanmlab <daanmlab@gmail.com>
2025-02-10 08:36:57 +0100

    Enhance ProtectedRoute and isAuthenticated middleware to support two-factor authentication flow

--- frontend/src/utils/middleware/ProtectedRoute.tsx
+++ frontend/src/utils/middleware/ProtectedRoute.tsx
@@ -1,9 +1,29 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import getAuthData from "./isAuthenticated";
 
 const ProtectedRoute: React.FC = () => {
-  const { isAuthenticated } = getAuthData();
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+  const location = useLocation();
+  const { isAuthenticated, needsTwoFactorAuthentication } = getAuthData();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  if (
+    needsTwoFactorAuthentication &&
+    location.pathname !== "/2fa/authenticate"
+  ) {
+    return <Navigate to="/2fa/authenticate" />;
+  }
+
+  if (
+    !needsTwoFactorAuthentication &&
+    location.pathname === "/2fa/authenticate"
+  ) {
+    return <Navigate to="/" />;
+  }
+
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
