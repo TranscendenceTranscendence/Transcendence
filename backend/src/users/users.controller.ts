c53a7b93229ce62b2ec0970d009ed9bd75fe446d
daanmlab <daanmlab@gmail.com>
2025-02-22 00:32:32 +0100

    Update users.controller.ts

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -101,7 +101,7 @@ export class UsersController {
   })
   @ApiResponse({ status: 404, description: 'User not found.' })
   @UseGuards(JwtAccessAuthGuard)
-  async me(@Req() req: Request): Promise<User> {
+  async me(@Req() req: Request): Promise<MeResponseSuccess> {
     console.log('req.user.id', req.user.id);
     try {
       const user = req.user;
--- frontend/src/generated-api/models/index.ts
+++ frontend/src/generated-api/models/index.ts
@@ -16,7 +16,6 @@ export * from "./FileUploadResponseSuccess";
 export * from "./Friend";
 export * from "./Game";
 export * from "./GetFriendRequestsDto";
-export * from "./MeResponseSuccess";
 export * from "./UpdateAchievementDto";
 export * from "./UpdateChatParticipantDto";
 export * from "./UpdateUserDto";
