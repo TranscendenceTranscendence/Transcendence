51a8b6f1ffd391ac179f9763da02aede81da3dd4
Ruben Finnerud <rubenfin@MacBook-Pro-van-Ruben.local>
2025-02-24 13:18:37 +0100

    fixed some linting issues in app-sidebar.tsx

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -1,33 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { useApi } from "@/utils/api";
+// import { useApi } from "@/utils/api";
 import { Button } from "@/components/ui/button";
 import { SettingsIcon } from "lucide-react";
 import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
 import AvatarDisplay from "../pages/updateUser/components/AvatarDisplay";
 import { Avatar } from "@/components/ui/avatar";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { useUser } from "@/utils/providers/UserProvider";
 
-class User {
-  id: number;
-  nickname: string;
-  email: string;
-  ladderLevel: number;
-  avatar: string;
-  constructor({ id, nickname, email, ladderLevel, avatar }: User) {
-    this.id = id;
-    this.nickname = nickname;
-    this.email = email;
-    this.ladderLevel = ladderLevel;
-    this.avatar = avatar;
-  }
-}
+// class User {
+//   id: number;
+//   nickname: string;
+//   email: string;
+//   ladderLevel: number;
+//   avatar: string;
+//   constructor({ id, nickname, email, ladderLevel, avatar }: User) {
+//     this.id = id;
+//     this.nickname = nickname;
+//     this.email = email;
+//     this.ladderLevel = ladderLevel;
+//     this.avatar = avatar;
+//   }
+// }
 
 export function AppSidebar() {
   {
     const me = useUser();
-    const api = useApi();
+    // const api = useApi();
 
     if (!me.user) {
       return <div>Loading...</div>;
