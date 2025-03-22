19f0af71d806af666f204998f77dbd02416e3953
jackdehaan <jack.de.haan@icloud.com>
2025-03-22 16:03:05 +0100

    Uncommented eslint file

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,34 +1,34 @@
-// /* eslint-disable */
+/* eslint-disable */
 
-// module.exports = {
-//   settings: {
-//     react: {
-//       version: "detect",
-//     },
-//   },
-//   env: {
-//     browser: true,
-//     es2021: true,
-//   },
-//   extends: [
-//     "eslint:recommended",
-//     "plugin:react/recommended",
-//     "plugin:react-hooks/recommended",
-//     "plugin:@typescript-eslint/recommended",
-//     "plugin:prettier/recommended",
-//   ],
-//   parser: "@typescript-eslint/parser",
-//   parserOptions: {
-//     ecmaVersion: 12,
-//     sourceType: "module",
-//     ecmaFeatures: {
-//       jsx: true,
-//     },
-//   },
-//   plugins: ["react", "react-hooks", "@typescript-eslint", "prettier"],
-//   rules: {
-//     "react/react-in-jsx-scope": "off",
-//     "react-hooks/exhaustive-deps": "off",
-//     "no-undef": "off",
-//   },
-// };
+module.exports = {
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["react", "react-hooks", "@typescript-eslint", "prettier"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react-hooks/exhaustive-deps": "off",
+    "no-undef": "off",
+  },
+};
