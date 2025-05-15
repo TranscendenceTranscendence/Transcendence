1dd6895bab4a22a007c73e34d96b1d7e6e752fd7
eunbi0308 <eunbicho1995@gmail.com>
2025-05-15 21:34:03 +0200

    profile navigation bug is fixed

--- frontend/src/pages/updateUser/updateUser.tsx
+++ frontend/src/pages/updateUser/updateUser.tsx
@@ -1,13 +1,13 @@
+import React from "react";
 import AvatarDisplay from "./components/AvatarDisplay";
+import { Link } from "react-router-dom";
 import ProfileForm from "./components/ProfileForm";
 import type { UpdateUserDto } from "../../generated-api";
 import { useCallback, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { useNavigate } from "react-router-dom";
 
 export default function UpdateUser() {
   const [avatarUrl, setAvatarUrl] = useState<string | undefined>(undefined);
-  const navigate = useNavigate();
 
   const onSend = useCallback((data: UpdateUserDto) => {
     setAvatarUrl(data.avatar);
@@ -24,7 +24,9 @@ export default function UpdateUser() {
       <div className="flex flex-col flex-grow items-start p-4 sm:p-10 w-full md:w-2/3">
         <div className="flex flex-row items-stretch w-full justify-between">
           <ProfileForm onSend={onSend} />
-          <Button onClick={() => navigate("/")}>Home</Button>
+          <Button className="p-4 rounded-xl w-48" asChild>
+            <Link to={`/`}>Home</Link>
+          </Button>
         </div>
       </div>
     </div>
